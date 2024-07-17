785a23208c2f9b433c16799745806d5f2339c11e
azeezabidoye <azeezabidoye@yahoo.com>
2024-07-17 13:35:37 +0100

    Components updated

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 function Post({ author, text }) {
+  const styleVar = {
+    color: "cyan",
+  };
   return (
     <div>
       <p style={{ color: "yellow" }}>Author: {author}</p>
-      <p>Message: {text}</p>
+      <p style={styleVar}>Message: {text}</p>
     </div>
   );
 }
