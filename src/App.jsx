2b6c01d7ee6221aacabc24939012def894d2bd77
azeezabidoye <azeezabidoye@yahoo.com>
2024-07-17 15:23:29 +0100

    New Components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PostList from "./components/PostsList";
+import NewPost from "./components/NewPost";
 
 function App() {
   return (
     <main>
       <h1>Posts</h1>
+      <NewPost />
       <PostList />
     </main>
   );
