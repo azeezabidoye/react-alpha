32585cdd3a1553685067970f9cc67c574e619ffc
azeezabidoye <azeezabidoye@yahoo.com>
2024-07-18 18:07:04 +0100

    New Component updated

---
+++ src/components/Modal.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from "./Modal.module.css";
+
+const Modal = ({ children, onClose }) => {
+  return (
+    <>
+      <div className={classes.backdrop} onClick={onClose} />
+      <dialog className={classes.modal} open>
+        {children}
+      </dialog>
+    </>
+  );
+};
+
+export default Modal;
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
 const PostList = () => {
   const [enteredAuthor, setEnteredAuthor] = useState("");
   const [enteredBody, setEnteredBody] = useState("");
+  const [modalIsVisible, setModalIsVisible] = useState(true);
 
   function handleAuthorChange(e) {
     setEnteredAuthor(e.target.value);
@@ -14,12 +16,20 @@ const PostList = () => {
     setEnteredBody(e.target.value);
   }
 
+  function handleHideModal() {
+    setModalIsVisible(false);
+  }
+
   return (
     <>
-      <NewPost
-        onAuthorChange={handleAuthorChange}
-        onBodyChange={handleBodyChange}
-      />
+      {modalIsVisible && (
+        <Modal onClose={handleHideModal}>
+          <NewPost
+            onAuthorChange={handleAuthorChange}
+            onBodyChange={handleBodyChange}
+          />
+        </Modal>
+      )}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} text={enteredBody} />
       </ul>
