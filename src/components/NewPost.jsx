5b5488115ec11303c9764ebd8052a64ef4c55649
azeezabidoye <azeezabidoye@yahoo.com>
2024-07-18 12:50:26 +0100

    New Component updated

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import classes from "./NewPost.module.css";
 
 function NewPost() {
+  const [newText, setNewText] = useState("");
+  function handleTextChange(e) {
+    setNewText(e.target.value);
+  }
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={handleTextChange} />
       </p>
+      <p>Current text update: {newText}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
