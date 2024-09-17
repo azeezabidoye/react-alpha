cc2e0a9ab9debf0792a42c3b8fa7fd8b706c8520
azeezabidoye <azeezabidoye@yahoo.com>
2024-09-17 18:08:28 +0100

    Components updated

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -15,7 +15,7 @@ const PostList = () => {
   }, []);
 
   function handleAddPost(postData) {
-    fetch("http://localhost:8080/posts/new", {
+    fetch("http://localhost:8080/posts/ne", {
       method: "POST",
       body: JSON.stringify(postData),
       headers: {
