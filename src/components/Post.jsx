9031164a737a855d6d28b403da54e6ddbcef7865
azeezabidoye <azeezabidoye@yahoo.com>
2025-01-24 18:19:12 +0100

    Component updated

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,7 +4,7 @@ import classes from "./Post.module.css";
 function Post({ author, text }) {
   return (
     <div className={classes.post}>
-      <p className={classes.author}>Author: {author}</p> <br />
+      <p className={classes.author}>Author: {author}</p>
       <p className={classes.text}>Message: {text}</p>
     </div>
   );
