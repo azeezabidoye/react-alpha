1316a5f9681a748ca4d306f774ecf0b771306ab5
azeezabidoye <azeezabidoye@yahoo.com>
2025-01-24 18:08:23 +0100

    Component updated

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -8,7 +8,7 @@ function MainHeader() {
     <header className={classes.header}>
       <h1 className={classes.logo}>
         <MdMessage />
-        React Poster
+        New React Poster
       </h1>
       <p>
         <Link to="/create-post" className={classes.button}>
