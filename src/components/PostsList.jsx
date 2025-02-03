d7f5e0172fe6f80540402c599d61e18497bb8ad1
azeezabidoye <azeezabidoye@yahoo.com>
2025-02-03 07:43:34 +0100

    components updated

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -16,7 +16,7 @@ const PostList = () => {
 
   function handleAddPost(postData) {
     fetch("http://localhost:8080/posts/", {
-      method: "G",
+      method: "POST",
       body: JSON.stringify(postData),
       headers: {
         "Content-Type": "application/json",
